2adfaeb7bd4e533cdb07a18ce36dad8890b09ac6
kartik <xkarrttik@gmail.com>
2024-05-26 17:41:01 +0530

    distanceCalculator

---
+++ src/understanding_fundamental_byQuestions/distanceCalculator.java
@@ -0,0 +1,11 @@
+package understanding_fundamental_byQuestions;
+
+//Assume a suitable value for distance between two cities (in km.). Write a java program
+//to convert and print this distance in meters, feet, inches and centimeters.
+
+
+public class distanceCalculator {
+    public static void main(String[] args){
+        System.out.println("distanceCalculator");
+    }
+}
