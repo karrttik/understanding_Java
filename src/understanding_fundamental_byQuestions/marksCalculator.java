e4fe2e0a10b54d36568ebc60cf3278c198278458
karrttik <kartikkumarrr@hotmail.com>
2024-05-28 21:13:22 +0530

    marksCalculator.

---
+++ src/understanding_fundamental_byQuestions/marksCalculator.java
@@ -0,0 +1,7 @@
+package understanding_fundamental_byQuestions;
+
+//Assume suitable values for marks obtained by a student in five different subjects are input through the keyboard.
+//Write a java program to find the aggregate marks and percentage marks
+
+public class marksCalculator {
+}
